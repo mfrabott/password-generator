67cc3adde253ab904aa883d14c9f9bdc1c1370c2
mfrabott <mfrabott14@gmail.com>
2022-10-26 22:20:10 -0400

    Updated README and added comments to js code.

--- README.md
+++ README.md
@@ -34,67 +34,14 @@ WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
 ```
 
-## Mock-Up
+## Reflection
 
-The following image shows the web application's appearance and functionality:
+As indicated above, I was presented with some starter code and asked to code a password generator meeting the above listed criteria.
+This project required me to break down the challenge into smaller pieces and write code specific to each spec. I ran into problems with getting the program to run more than once until I added a function to clear the previous results. I also had to find ways to evaluate whether the user selected the appropriate criteria and to create opportunities for the user to correct invalid inputs. I also found out how to allow the user to escape at the number selection prompts for improved user experience.
 
-![The Password Generator application displays a red button to "Generate Password".](./Assets/03-javascript-homework-demo.png)
+## Deployed App Website
 
-## Grading Requirements
 
-> **Note**: If a Challenge assignment submission is marked as “0”, it is considered incomplete and will not count towards your graduation requirements. Examples of incomplete submissions include the following:
->
-> * A repository that has no code
->
-> * A repository that includes a unique name but nothing else
->
-> * A repository that includes only a README file but nothing else
->
-> * A repository that only includes starter code
+## Screenshot
 
-This Challenge is graded based on the following criteria: 
 
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the preceding acceptance criteria.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the Challenge instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository, with a unique name and a readme describing the project.
-
-- - -
-© 2022 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
--- script.js
+++ script.js
@@ -13,35 +13,44 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Potential character sets to include in password.
 var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r","s", "t", "u", "v", "w", "x", "y", "z"];
 var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R","S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", "_"];
+
+// Empty variable to collect user choice of password length.
 var numUserChars = "";
 
+// Collects user inputs for password length and character sets to include.
 var userInputs =  function() { 
     
-    numUserChars = prompt("\n\nHow many characters would you like to include in your password? \nPlease enter a number between 8 and 128:");          
+    numUserChars = prompt("\n\nHow many characters would you like to include in your password? \nPlease enter a number between 8 and 128 below:\n");
+    
+    // Clears out of the program if 'Cancel' is selected in prompt box.
     if (numUserChars === null) {
         return
     }
 
+    // Evaluates if chosen number is valid. So long as selection is invalid, new prompt is issued.
     while (numUserChars <8 || numUserChars>128) { 
-        numUserChars = prompt("\n\nYour selection is invalid. \nPlease enter a number between 8 and 128:");
+        numUserChars = prompt("\n\nYour password must contain between 8 and 128 characters. \nPlease enter a number between 8 and 128 below:\n");
+        
         if (numUserChars === null) {
             return
         }
     }
 
+    // Evaluates length of user character pool to determine if any charater sets have been selected. At least one set must be selected to exit while loop. For any selected character set, said array is added to userCharPool. 
     while (userCharPool.length <1) { 
-        alert("\n\nPlease choose at least one set of characters to include in your password from the following prompts.");
-        if (confirm("\n\nWould you like to include lowercase characters in your password? \n'OK' for yes. \n'Cancel' for no. ")){
+        alert("\n\nPlease choose at least one set of characters to include in your password from the following prompts.\n");
+        if (confirm("\n\nWould you like to include lowercase characters in your password? \n'OK' for yes. \n'Cancel' for no.\n")){
             userCharPool = userCharPool.concat(lowerLetters);
         }
-        if (confirm("\n\nWould you like to include uppercase characters in your password? \n'OK' for yes. \n'Cancel' for no. ")){
+        if (confirm("\n\nWould you like to include uppercase characters in your password? \n'OK' for yes. \n'Cancel' for no.\n")){
             userCharPool = userCharPool.concat(upperLetters);
         }
-        if (confirm("\n\nWould you like to include numbers in your password? \n'OK' for yes. \n'Cancel' for no. ")){
+        if (confirm("\n\nWould you like to include numbers in your password? \n'OK' for yes. \n'Cancel' for no.\n")){
             userCharPool = userCharPool.concat(numbers);
         }
         if (confirm("\n\nWould you like to include special characters in your password? \n'OK' for yes. \n'Cancel' for no. ")){
@@ -50,6 +59,7 @@ var userInputs =  function() {
     }
 }
 
+// Selects random characters from the userCharPool * number selected by user and produces as a password string.
 var randomizePassword = function() {
     function randomNumber(max) {
         return Math.floor(Math.random() * max);
@@ -60,12 +70,14 @@ var randomizePassword = function() {
     stringPassword = securePassword.join("");
 }
 
+// Clears variables so that the program can continue to run.
 var clear = function() {
     userCharPool = [];
     securePassword = [];
     stringPassword = "";
 }
 
+// Executes steps of password generator.
 var generatePassword = function(){
     clear();
     userInputs(); 
